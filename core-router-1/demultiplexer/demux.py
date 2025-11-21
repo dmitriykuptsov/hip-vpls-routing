2e0ae64ed1f1a2897298cf4fd27b6e7a7c0eef7c
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-11-21 19:46:09 +0500

    Reverted to working code

--- core-router-1/demultiplexer/demux.py
+++ core-router-1/demultiplexer/demux.py
@@ -52,8 +52,8 @@ class Demultiplexer():
         self.socket_in = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_IP)
         self.socket_in.bind((own_interface, 0x0800))
         self.socket_out = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        #self.socket_out.bind((own_ip, 0))
         self.socket_out.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1);
-
         for interface in self.interfaces:
             network = Misc.ipv4_address_to_int(interface["address"]) & Misc.ipv4_address_to_int(interface["mask"])
             self.routing_table[Misc.bytes_to_ipv4_string(Misc.int_to_ipv4_address(network))] = (interface["destination"], interface["auth"]);
