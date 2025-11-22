d29d2876c165b2f4a54bc969be56c79d9eb9c369
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-11-22 16:01:07 +0500

    Reverted changes

--- core-router-1/demultiplexer/demux.py
+++ core-router-1/demultiplexer/demux.py
@@ -49,17 +49,16 @@ class Demultiplexer():
         self.own_ip = own_ip
         ETH_P_ALL = 3
         #self.socket_in = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_IP)
-        #self.socket_in = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
-        #self.socket_in.bind((own_interface, 0x0800))
-        #self.socket_out = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-        self.socket_out = socket.socket(socket.AF_INET, socket.SOCK_RAW, GRE.GRE_PROTOCOL_NUMBER)
-        self.socket_out.bind((own_ip, 0))
+        self.socket_in = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+        self.socket_in.bind((own_interface, 0x0800))
+        self.socket_out = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        #self.socket_out.bind((own_ip, 0))
         self.socket_out.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1);
         for interface in self.interfaces:
             network = Misc.ipv4_address_to_int(interface["address"]) & Misc.ipv4_address_to_int(interface["mask"])
             self.routing_table[Misc.bytes_to_ipv4_string(Misc.int_to_ipv4_address(network))] = (interface["destination"], interface["auth"]);
 
-        thread = threading.Thread(target=self.read, args=(self.socket_out, self.socket_out), daemon=True)
+        thread = threading.Thread(target=self.read, args=(self.socket_in, self.socket_out), daemon=True)
         thread.start()
 
     def set_key(self, src, dst, key):
